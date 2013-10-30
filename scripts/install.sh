ae00a14f74b39ae5aaa68bc967632002b1bdba0e
Marek Ventur <marekventur@gmail.com>
2013-10-30 10:19:06 +0000

    Typo

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ mkdir /var/log/rohrpost || :
 mkdir /etc/rohrpost || :
 cp -r config/* /etc/rohrpost/ || :
 rm /etc/rohrpost/README.md || :
-chown rohrost /var/log/rohrpost
+chown rohrpost /var/log/rohrpost
 chmod a+w -R /var/log/rohrpost
 chmod a+w -R /etc/rohrpost
 
